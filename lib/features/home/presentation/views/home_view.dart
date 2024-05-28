d0dbfadfd9bbe40805212b9cadd87c1120f71a23
omareldery <105989767+omareldery@users.noreply.github.com>
2024-05-28 23:26:54 +0300

    ..

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,13 +1,38 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:floating_menu_panel/floating_menu_panel.dart';
+import 'package:iconly/iconly.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<IconData>? icons = const [
+    IconlyBold.bookmark,
+    IconlyBold.search,
+    IconlyBold.setting,
+    IconlyBold.camera,
+  ];
+  final int _selectedIndex = 3;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeViewBody(),
+    return Scaffold(
+      body: Stack(children: [
+         const HomeViewBody(),
+        FloatingMenuPanel(
+          buttons: icons,
+            onPressed: (int ) {
+            },
+          backgroundColor: Colors.amber,
+          borderColor: Colors.black,
+          borderWidth: 2,
+        ),
+
+      ]),
     );
   }
 }
